b16d050289cf0deb5c2e2e3c2e64c7d46a756585
iliass-de <iliasshlm@gmail.com>
2023-02-25 22:31:21 +0100

    feat: add user and product models

---
+++ src/models/product.js
@@ -0,0 +1,11 @@
+export default class Product {
+    constructor(id, name, description, price, availableQuantity) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.availableQuantity = availableQuantity;
+    }
+  }
+
+
