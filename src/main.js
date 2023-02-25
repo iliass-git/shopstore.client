d832904fc5b6d9ca6c034973316ec26e0cf1406d
iliass-de <iliasshlm@gmail.com>
2023-02-25 22:55:01 +0100

    feat: register the auth service

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
 import App from './App.vue'
+import Vue from 'vue';
+import store from './store';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-createApp(App).mount('#app')
+
+new Vue({
+    store,
+    render: h => h(App)
+  }).$mount('#app');
